d8398045db4dd6cc2527a676a6131ed431dbeb0e
TobiaszMartyniuk <166758356+TobiaszMartyniuk@users.noreply.github.com>
2025-03-22 02:10:43 +0100

    Almost ready, yet to add ships

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ public class OverfillException : Exception
     public OverfillException(string message) : base(message) { }
 }
 
+public class WrongContentsException : Exception
+{
+    public WrongContentsException(string message) : base(message) { }
+}
+
 public abstract class Container
 {
     private static int counter = 1;
@@ -21,6 +26,7 @@ public abstract class Container
     public double Height { get; }
     public double Depth { get; }
     public double MaxCapacity { get; }
+    public string Contents { get; set; }
 
     protected Container(string type, double ownWeight, double height, double depth, double maxCapacity)
     {
@@ -28,6 +34,7 @@ public abstract class Container
         Height = height;
         Depth = depth;
         MaxCapacity = maxCapacity;
+        Contents = "empty";
 
         
         SerialNumber = GenerateUniqueSerialNumber(type);
@@ -40,23 +47,27 @@ public abstract class Container
         return serial;
     }
 
-    public virtual void Load(double mass)
+    public virtual void Load(double mass, string contents)
     {
         if (LoadMass + mass > MaxCapacity)
         {
             throw new OverfillException($"Nie można załadować {mass} kg. Przekroczona maksymalna ładowność kontenera {SerialNumber}.");
+        }else if(contents != Contents && Contents != "empty") {
+            throw new WrongContentsException($"Nie można załadować {contents} do kontenera: {SerialNumber}, ponieważ zawiera on już produkty: {Contents}");
         }
         LoadMass += mass;
+        Contents = contents;
     }
 
-    public virtual void Unload(double mass)
+    public virtual void Unload()
     {
-        LoadMass -= mass;
+        LoadMass = 0;
+        Contents = "empty";
     }
 
     public override string ToString()
     {
-        return $"Numer: {SerialNumber}, Ładunek: {LoadMass}/{MaxCapacity} kg, Waga własna: {OwnWeight} kg, Wymiary: {Height}x{Depth} cm";
+        return $"Numer: {SerialNumber}, Ładunek: {Contents}, {LoadMass}/{MaxCapacity} kg, Waga własna: {OwnWeight} kg, Wymiary: {Height}x{Depth} cm";
     }
 }
 
@@ -65,7 +76,7 @@ public class LiquidContainer: Container, IHazardNotifier{
     public LiquidContainer(double ownWeight, double height, double depth, double maxCapacity, bool isHazardous) : base("L", ownWeight, height, depth, maxCapacity){
         IsHazardous = isHazardous;
     }
-    public override void Load(double mass){
+    public override void Load(double mass, string contents){
         double allowedCapacity = IsHazardous ? MaxCapacity/2 : MaxCapacity * 0.9;
 
         if (LoadMass + mass > allowedCapacity){
@@ -74,29 +85,58 @@ public class LiquidContainer: Container, IHazardNotifier{
         }
         else
         {
-            base.Load(mass);
+            base.Load(mass, contents);
         }
     }
 
     public void Notify(){
-        Console.WriteLine($"Próba przekroczenia bezpiecznej ładowności kontenera {SerialNumber}.");
+        Console.WriteLine($"Próba przekroczenia bezpiecznej ładowności kontenera: {SerialNumber}.");
     }
 }
 
 public class GasContainer : Container, IHazardNotifier{
     public GasContainer(double ownWeight, double height, double depth, double maxCapacity) : base("G", ownWeight, height, depth, maxCapacity){}
 
-    public override void Unload(double mass){
-        if(mass > 0.95*LoadMass){
-            Notify();
-        }
-        else{
-            base.Unload(mass);
-        }
+    public override void Unload(){
+        LoadMass -= LoadMass*0.95;
+        Contents = "empty";
     }
 
     public void Notify(){
-        Console.WriteLine($"Nie można wyładować więcej niż 95% gazu z kontenera: {SerialNumber}");
+        Console.WriteLine($"Niebezpieczne działanie: {SerialNumber}");
+    }
+}
+
+public class RefridgeratorContainer: Container, IHazardNotifier{
+
+    private static Dictionary<string, double> optimalTemperatures = new Dictionary<string, double>
+    {
+        {"Bananas", 13.3},
+        {"Chocolate", 18},
+        {"Fish", 2},
+        {"Meat", -15},
+        {"Ice cream", -18},
+        {"Frozen pizza", -30},
+    };
+
+    public double Temperature { get; set; }
+    public RefridgeratorContainer(double ownWeight, double height, double depth, double maxCapacity, double temperature) : base("C", ownWeight, height, depth, maxCapacity)
+    {
+        Temperature = temperature;
+    }
+
+    public override void Load(double mass, string contents)
+    {
+        if(!optimalTemperatures.ContainsKey(contents))
+        {
+            Console.WriteLine($"{contents} nie jest na liście produktów");
+        }else if(optimalTemperatures[contents] < Temperature){
+            Notify();
+        }else base.Load(mass, contents);
+    }
+
+    public void Notify(){
+        Console.WriteLine($"Zła temperatura dla zawartości: {SerialNumber}");
     }
 }
 
@@ -106,16 +146,27 @@ public class Program
     {
         try
         {
-           LiquidContainer container1 = new LiquidContainer(100, 20, 20, 2000, true);
-           LiquidContainer container2 = new LiquidContainer(100, 20, 20, 2000, false);
-           container2.Load(1000);
-           container2.Load(1100);
+           Container containerC = new RefridgeratorContainer(100, 150, 150, 2000, 14);
+           Container containerG = new GasContainer(100, 150, 150, 2000);
+           Container containerL = new LiquidContainer(100, 150, 150, 2000, true);
 
-           GasContainer container3 = new GasContainer(100, 20, 20, 1000);
-           container3.Load(1000);
-           container3.Unload(950);
+           containerC.Load(200, "Bananas");
+           Console.WriteLine($"{containerC}");
+
+           containerG.Load(200, "Helium");
+           containerG.Unload();
+           Console.WriteLine($"{containerG}");
+
+           containerL.Load(1900, "Rocket Fuel");
+           containerL.Load(1000, "Rocket Fuel");
+           Console.WriteLine($"{containerL}");
+           
+
         }
         catch (OverfillException ex)
+        {
+            Console.WriteLine($"Błąd: {ex.Message}");
+        }catch (WrongContentsException ex)
         {
             Console.WriteLine($"Błąd: {ex.Message}");
         }
